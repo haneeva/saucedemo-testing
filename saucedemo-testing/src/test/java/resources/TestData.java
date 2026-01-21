635875c39f14a78e9918aa4d7f614da41afd3050
Asma Hanifah <asmahnh95@gmail.com>
2026-01-21 21:26:01 +0700

    login testing

---
+++ saucedemo-testing/src/test/java/resources/TestData.java
@@ -0,0 +1,12 @@
+package resources;
+
+public class TestData {
+    
+    public static final String BASE_URL ="https://www.saucedemo.com/";
+    public static final String USERNAME ="standard_user";
+    public static final String PASSWORD ="secret_sauce";
+    public static final String INVALID_USERNAME ="standard";
+    public static final String INVALID_PASSWORD ="secret";
+    
+
+}
