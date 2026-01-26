a69c06f68e843a06589c41ade5f72924eba78375
Asma Hanifah <asmahnh95@gmail.com>
2026-01-26 12:28:00 +0700

    Add jenkins file

--- saucedemo-testing/src/test/java/base/BaseTest.java
+++ saucedemo-testing/src/test/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -26,13 +27,21 @@ public class BaseTest {
     @BeforeMethod
     public  void setUp(){
         WebDriverManager.chromedriver().setup(); // Setup Chromedriver using WebDriveManager
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");   // WAJIB untuk Jenkins
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
         if (driver == null){
             System.err.println("Driver is not initialized!");
         } else {
             System.out.println("Driver initialized successfully.");
         }
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get(TestData.BASE_URL);
 
